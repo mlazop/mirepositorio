3b62c29bd67bf82b04ebfd5977f2355920702be9
mlazop <162070092+mlazop@users.noreply.github.com>
2024-04-01 23:17:43 -0500

    jpa java

---
+++ jpaprueba/src/main/java/com/mycompany/jpaprueba/Jpaprueba.java
@@ -0,0 +1,12 @@
+package com.mycompany.jpaprueba;
+
+import com.mycompany.jpaprueba.persistencia.ControladoraPersistencia;
+
+public class Jpaprueba {
+
+    public static void main(String[] args) {
+        
+        ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+       
+    }
+}
---
+++ jpaprueba/src/main/java/com/mycompany/jpaprueba/persistencia/ControladoraPersistencia.java
@@ -0,0 +1,6 @@
+
+package com.mycompany.jpaprueba.persistencia;
+
+public class ControladoraPersistencia {
+    AlumnoJpaController aluJpa = new AlumnoJpaController();
+}
